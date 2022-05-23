35c5ff8b96d17b35069de7dbd65ba50ce02e9e70
lukwol <wolanczyk.lukasz@gmail.com>
2022-05-23 10:43:07 +0200

    Tweak constants

--- domain/src/main/kotlin/com/diskusage/domain/common/Constants.kt
+++ domain/src/main/kotlin/com/diskusage/domain/common/Constants.kt
@@ -2,6 +2,6 @@ package com.diskusage.domain.common
 
 object Constants {
     const val ArcWidth = 100f
-    const val MaxChartDepth = 7
-    const val DiskEntrySizeFilterThreshold = 0.05f
+    const val MaxChartDepth = 10
+    const val DiskEntrySizeFilterThreshold = 0.005f
 }
