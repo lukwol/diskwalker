31896a8482a886ecafbda04d404d57b75290a003
lukwol <wolanczyk.lukasz@gmail.com>
2022-05-29 11:54:36 +0200

    Use ArcStubs for IsArcSelectedTest

--- domain/src/test/kotlin/com/diskusage/domain/stubs/stubs/ArcStubs.kt
+++ domain/src/test/kotlin/com/diskusage/domain/stubs/stubs/ArcStubs.kt
@@ -3,9 +3,29 @@ package com.diskusage.domain.stubs.stubs
 import com.diskusage.domain.entities.Arc
 
 object ArcStubs {
-    val arc = Arc(
-        startAngle = 120.0f,
-        sweepAngle = 45.0f,
-        depth = 2.0f
+    val hidden = Arc(
+        startAngle = 0f,
+        sweepAngle = 360f,
+        depth = 0f
+    )
+    val selected = Arc(
+        startAngle = 0f,
+        sweepAngle = 360f,
+        depth = 1f
+    )
+    val child1 = Arc(
+        startAngle = 0f,
+        sweepAngle = 45f,
+        depth = 2f
+    )
+    val child2 = Arc(
+        startAngle = 45f,
+        sweepAngle = 60f,
+        depth = 2f
+    )
+    val far = Arc(
+        startAngle = 30f,
+        sweepAngle = 60f,
+        depth = 8f
     )
 }
--- domain/src/test/kotlin/com/diskusage/domain/stubs/usecases/chart/arc/IsArcSelectedTest.kt
+++ domain/src/test/kotlin/com/diskusage/domain/stubs/usecases/chart/arc/IsArcSelectedTest.kt
@@ -2,11 +2,10 @@ package com.diskusage.domain.stubs.usecases.chart.arc
 
 import androidx.compose.ui.geometry.Offset
 import com.diskusage.domain.di.domainModule
-import com.diskusage.domain.entities.Arc
+import com.diskusage.domain.stubs.stubs.ArcStubs
 import com.diskusage.domain.usecases.chart.chartitem.arc.IsArcSelected
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
@@ -30,41 +29,30 @@ class IsArcSelectedTest : KoinTest {
     @Nested
     inner class HiddenDiskEntryArc {
 
-        private lateinit var arc: Arc
-
-        @BeforeEach
-        internal fun setUp() {
-            arc = Arc(
-                startAngle = 0f,
-                sweepAngle = 360f,
-                depth = 0f
-            )
-        }
-
         @Test
         fun `close offset`() {
-            isArcSelected(arc = arc, offset = closeOffset) shouldBe false
+            isArcSelected(arc = ArcStubs.hidden, offset = closeOffset) shouldBe false
         }
 
         @Test
         fun `far offset`() {
-            isArcSelected(arc = arc, offset = farOffset) shouldBe false
+            isArcSelected(arc = ArcStubs.hidden, offset = farOffset) shouldBe false
         }
 
         @Test
         fun `zero offset`() {
-            isArcSelected(arc = arc, offset = Offset.Zero) shouldBe false
+            isArcSelected(arc = ArcStubs.hidden, offset = Offset.Zero) shouldBe false
         }
 
         @Test
         fun `infinite offset`() {
-            isArcSelected(arc = arc, offset = Offset.Infinite) shouldBe false
+            isArcSelected(arc = ArcStubs.hidden, offset = Offset.Infinite) shouldBe false
         }
 
         @Test
         fun `unspecified offset`() {
             shouldThrow<java.lang.IllegalStateException> {
-                isArcSelected(arc = arc, offset = Offset.Unspecified)
+                isArcSelected(arc = ArcStubs.hidden, offset = Offset.Unspecified)
             }
         }
     }
@@ -72,41 +60,30 @@ class IsArcSelectedTest : KoinTest {
     @Nested
     inner class SelectedDiskEntryArc {
 
-        private lateinit var arc: Arc
-
-        @BeforeEach
-        internal fun setUp() {
-            arc = Arc(
-                startAngle = 0f,
-                sweepAngle = 360f,
-                depth = 1f
-            )
-        }
-
         @Test
         fun `close offset`() {
-            isArcSelected(arc = arc, offset = closeOffset) shouldBe false
+            isArcSelected(arc = ArcStubs.selected, offset = closeOffset) shouldBe false
         }
 
         @Test
         fun `far offset`() {
-            isArcSelected(arc = arc, offset = farOffset) shouldBe false
+            isArcSelected(arc = ArcStubs.selected, offset = farOffset) shouldBe false
         }
 
         @Test
         fun `zero offset`() {
-            isArcSelected(arc = arc, offset = Offset.Zero) shouldBe true
+            isArcSelected(arc = ArcStubs.selected, offset = Offset.Zero) shouldBe true
         }
 
         @Test
         fun `infinite offset`() {
-            isArcSelected(arc = arc, offset = Offset.Infinite) shouldBe false
+            isArcSelected(arc = ArcStubs.selected, offset = Offset.Infinite) shouldBe false
         }
 
         @Test
         fun `unspecified offset`() {
             shouldThrow<java.lang.IllegalStateException> {
-                isArcSelected(arc = arc, offset = Offset.Unspecified)
+                isArcSelected(arc = ArcStubs.selected, offset = Offset.Unspecified)
             }
         }
     }
@@ -114,41 +91,30 @@ class IsArcSelectedTest : KoinTest {
     @Nested
     inner class CloseDiskEntryArc {
 
-        private lateinit var arc: Arc
-
-        @BeforeEach
-        internal fun setUp() {
-            arc = Arc(
-                startAngle = 30f,
-                sweepAngle = 90f,
-                depth = 2f
-            )
-        }
-
         @Test
         fun `close offset`() {
-            isArcSelected(arc = arc, offset = closeOffset) shouldBe true
+            isArcSelected(arc = ArcStubs.child2, offset = closeOffset) shouldBe true
         }
 
         @Test
         fun `far offset`() {
-            isArcSelected(arc = arc, offset = farOffset) shouldBe false
+            isArcSelected(arc = ArcStubs.child2, offset = farOffset) shouldBe false
         }
 
         @Test
         fun `zero offset`() {
-            isArcSelected(arc = arc, offset = Offset.Zero) shouldBe false
+            isArcSelected(arc = ArcStubs.child2, offset = Offset.Zero) shouldBe false
         }
 
         @Test
         fun `infinite offset`() {
-            isArcSelected(arc = arc, offset = Offset.Infinite) shouldBe false
+            isArcSelected(arc = ArcStubs.child2, offset = Offset.Infinite) shouldBe false
         }
 
         @Test
         fun `unspecified offset`() {
             shouldThrow<java.lang.IllegalStateException> {
-                isArcSelected(arc = arc, offset = Offset.Unspecified)
+                isArcSelected(arc = ArcStubs.child1, offset = Offset.Unspecified)
             }
         }
     }
@@ -156,80 +122,47 @@ class IsArcSelectedTest : KoinTest {
     @Nested
     inner class FarDiskEntryArc {
 
-        private lateinit var arc: Arc
-
-        @BeforeEach
-        internal fun setUp() {
-            arc = Arc(
-                startAngle = 30f,
-                sweepAngle = 60f,
-                depth = 8f
-            )
-        }
-
         @Test
         fun `close offset`() {
-            isArcSelected(arc = arc, offset = closeOffset) shouldBe false
+            isArcSelected(arc = ArcStubs.far, offset = closeOffset) shouldBe false
         }
 
         @Test
         fun `far offset`() {
-            isArcSelected(arc = arc, offset = farOffset) shouldBe true
+            isArcSelected(arc = ArcStubs.far, offset = farOffset) shouldBe true
         }
 
         @Test
         fun `zero offset`() {
-            isArcSelected(arc = arc, offset = Offset.Zero) shouldBe false
+            isArcSelected(arc = ArcStubs.far, offset = Offset.Zero) shouldBe false
         }
 
         @Test
         fun `infinite offset`() {
-            isArcSelected(arc = arc, offset = Offset.Infinite) shouldBe false
+            isArcSelected(arc = ArcStubs.far, offset = Offset.Infinite) shouldBe false
         }
 
         @Test
         fun `unspecified offset`() {
             shouldThrow<java.lang.IllegalStateException> {
-                isArcSelected(arc = arc, offset = Offset.Unspecified)
+                isArcSelected(arc = ArcStubs.far, offset = Offset.Unspecified)
             }
         }
     }
 
     @Nested
     inner class EdgeCases {
-        private lateinit var arc0: Arc
-        private lateinit var arc1: Arc
-        private lateinit var arc2: Arc
-
-        @BeforeEach
-        internal fun setUp() {
-            arc0 = Arc(
-                startAngle = 0f,
-                sweepAngle = 360f,
-                depth = 1f
-            )
-            arc1 = Arc(
-                startAngle = 0f,
-                sweepAngle = 45f,
-                depth = 2f
-            )
-            arc2 = Arc(
-                startAngle = 45f,
-                sweepAngle = 60f,
-                depth = 2f
-            )
-        }
 
         @Test
         fun `between arc1 and arc2`() {
-            isArcSelected(arc = arc1, offset = closeOffset) shouldBe false
-            isArcSelected(arc = arc2, offset = closeOffset) shouldBe true
+            isArcSelected(arc = ArcStubs.child1, offset = closeOffset) shouldBe false
+            isArcSelected(arc = ArcStubs.child2, offset = closeOffset) shouldBe true
         }
 
         @Test
         fun `between arc0 and arc2`() {
-            isArcSelected(arc = arc0, offset = Offset(0f, 100f)) shouldBe false
-            isArcSelected(arc = arc2, offset = Offset(0f, 100f)) shouldBe true
+            isArcSelected(arc = ArcStubs.selected, offset = Offset(0f, 100f)) shouldBe false
+            isArcSelected(arc = ArcStubs.child2, offset = Offset(0f, 100f)) shouldBe true
         }
     }
 }
