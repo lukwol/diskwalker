25df9ad9cc3920853168814719bb7c24a99ad2cd
lukwol <wolanczyk.lukasz@gmail.com>
2022-05-30 09:53:01 +0200

    Add IncludeDiskEntryTest

---
+++ domain/src/test/kotlin/com/diskusage/domain/stubs/usecases/chart/IncludeDiskEntryTest.kt
@@ -0,0 +1,71 @@
+package com.diskusage.domain.stubs.usecases.chart
+
+import com.diskusage.domain.di.domainModule
+import com.diskusage.domain.entities.DiskEntry
+import com.diskusage.domain.stubs.stubs.DiskEntryStubs
+import com.diskusage.domain.usecases.chart.IncludeDiskEntry
+import com.diskusage.domain.usecases.diskentry.GetDepth
+import com.diskusage.domain.usecases.diskentry.GetRoot
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockkClass
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import org.koin.test.junit5.KoinTestExtension
+import org.koin.test.junit5.mock.MockProviderExtension
+import org.koin.test.mock.declareMock
+import java.nio.file.Path
+
+class IncludeDiskEntryTest : KoinTest {
+
+    private val includeDiskEntry by inject<IncludeDiskEntry>()
+    private lateinit var getDepth: GetDepth
+    private lateinit var getRoot: GetRoot
+
+    @JvmField
+    @RegisterExtension
+    val koinTestExtension = KoinTestExtension.create {
+        modules(domainModule)
+    }
+
+    @JvmField
+    @RegisterExtension
+    val mockProvider = MockProviderExtension.create { clazz ->
+        mockkClass(clazz)
+    }
+
+    @Test
+    fun `normal disk entry`() {
+        includeDiskEntry.invoke(DiskEntryStubs.dir2) shouldBe true
+        includeDiskEntry.invoke(DiskEntryStubs.file111, DiskEntryStubs.rootDir) shouldBe true
+    }
+
+    @Test
+    fun `too deep disk entry`() {
+        getDepth = declareMock {
+            every { this@declareMock(DiskEntryStubs.dir1, DiskEntryStubs.rootDir) } returns 11f
+        }
+        includeDiskEntry.invoke(DiskEntryStubs.dir1) shouldBe false
+        includeDiskEntry.invoke(DiskEntryStubs.dir1, DiskEntryStubs.rootDir) shouldBe false
+    }
+
+    @Test
+    fun `too small disk entry`() {
+        val fooFile = DiskEntry.File(
+            name = "foo",
+            path = Path.of("/dir/dir1/dir11/foo"),
+            parent = DiskEntryStubs.dir11,
+            size = 32,
+        )
+        getRoot = declareMock {
+            every { this@declareMock(fooFile) } returns DiskEntryStubs.rootDir
+        }
+        getDepth = declareMock {
+            every { this@declareMock(fooFile, DiskEntryStubs.rootDir) } returns 8f
+        }
+        includeDiskEntry.invoke(fooFile) shouldBe false
+        includeDiskEntry.invoke(fooFile, DiskEntryStubs.rootDir) shouldBe false
+    }
+}
