49f53f12d4749e0897336f2de51bfa2ac9cb4900
lukwol <wolanczyk.lukasz@gmail.com>
2022-05-30 08:23:38 +0200

    Add GetDiskEntriesTest

---
+++ domain/src/test/kotlin/com/diskusage/domain/stubs/usecases/chart/GetDiskEntriesTest.kt
@@ -0,0 +1,90 @@
+package com.diskusage.domain.stubs.usecases.chart
+
+import com.diskusage.domain.di.domainModule
+import com.diskusage.domain.stubs.stubs.DiskEntryStubs
+import com.diskusage.domain.usecases.chart.GetDiskEntries
+import com.diskusage.domain.usecases.chart.IncludeDiskEntry
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.mockk.every
+import io.mockk.mockkClass
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import org.koin.test.junit5.KoinTestExtension
+import org.koin.test.junit5.mock.MockProviderExtension
+import org.koin.test.mock.declareMock
+
+class GetDiskEntriesTest : KoinTest {
+
+    private val getDiskEntries by inject<GetDiskEntries>()
+    private lateinit var includeDiskEntry: IncludeDiskEntry
+
+    @JvmField
+    @RegisterExtension
+    val koinTestExtension = KoinTestExtension.create {
+        modules(domainModule)
+    }
+
+    @JvmField
+    @RegisterExtension
+    val mockProvider = MockProviderExtension.create { clazz ->
+        mockkClass(clazz)
+    }
+
+    @Nested
+    inner class WithExcludedItems {
+
+        @BeforeEach
+        internal fun setUp() {
+            includeDiskEntry = declareMock {
+                every { this@declareMock(not(DiskEntryStubs.dir11), any()) } returns true
+                every { this@declareMock(DiskEntryStubs.dir11, any()) } returns false
+            }
+        }
+
+        @Test
+        internal fun `disk entries list`() {
+            getDiskEntries(DiskEntryStubs.rootDir) shouldContainExactlyInAnyOrder listOf(
+                DiskEntryStubs.rootDir,
+                DiskEntryStubs.file1,
+                DiskEntryStubs.dir1,
+                DiskEntryStubs.file11,
+                DiskEntryStubs.file12,
+                DiskEntryStubs.dir2,
+                DiskEntryStubs.file21,
+                DiskEntryStubs.file22,
+            )
+        }
+    }
+
+    @Nested
+    inner class WithoutExcludedItems {
+
+        @BeforeEach
+        internal fun setUp() {
+            includeDiskEntry = declareMock {
+                every { this@declareMock(any(), any()) } returns true
+            }
+        }
+
+        @Test
+        internal fun `disk entries list`() {
+            getDiskEntries(DiskEntryStubs.rootDir) shouldContainExactlyInAnyOrder listOf(
+                DiskEntryStubs.rootDir,
+                DiskEntryStubs.file1,
+                DiskEntryStubs.dir1,
+                DiskEntryStubs.file11,
+                DiskEntryStubs.file12,
+                DiskEntryStubs.dir11,
+                DiskEntryStubs.file111,
+                DiskEntryStubs.file112,
+                DiskEntryStubs.dir2,
+                DiskEntryStubs.file21,
+                DiskEntryStubs.file22,
+            )
+        }
+    }
+}
