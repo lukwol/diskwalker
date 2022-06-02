1d38ac9698c84c7f01a5deef3766b2e626f58d7d
lukwol <wolanczyk.lukasz@gmail.com>
2022-06-02 09:58:44 +0200

    Add documentation for Arc, ChartItem and GetArc

--- domain/src/main/kotlin/com/diskusage/domain/entities/Arc.kt
+++ domain/src/main/kotlin/com/diskusage/domain/entities/Arc.kt
@@ -1,5 +1,14 @@
 package com.diskusage.domain.entities
 
+/**
+ * An arc specification that will be used for drawing.
+ *
+ * @property startAngle Starting angle in degrees. 0 represents 3 o'clock
+ * @property sweepAngle Size of the arc in degrees that is drawn clockwise relative to [startAngle]
+ * @property depth determines how far from center [Arc] is drawn
+ *
+ * @see androidx.compose.ui.graphics.drawscope.DrawScope.drawArc
+ */
 data class Arc(
     val startAngle: Float,
     val sweepAngle: Float,
--- domain/src/main/kotlin/com/diskusage/domain/usecases/chart/chartitem/arc/GetArc.kt
+++ domain/src/main/kotlin/com/diskusage/domain/usecases/chart/chartitem/arc/GetArc.kt
@@ -5,12 +5,23 @@ import com.diskusage.domain.entities.DiskEntry
 import com.diskusage.domain.usecases.diskentry.GetDepth
 import com.diskusage.domain.usecases.diskentry.GetRoot
 
+
+/**
+ *  Usecase for calculating [Arc]
+ */
 class GetArc(
     private val getStartAngle: GetStartAngle,
     private val getSweepAngle: GetSweepAngle,
     private val getDepth: GetDepth,
     private val getRoot: GetRoot,
 ) {
+    /**
+     * Calculate the [Arc] for [DiskEntry] starting from its root or specific [DiskEntry].
+     *
+     * @param diskEntry [DiskEntry] for which the [Arc] is calculated
+     * @param fromDiskEntry Currently selected [DiskEntry] from which calculation should start
+     * @return [Arc] with calculated start and sweep angles and depth
+     */
     operator fun invoke(
         diskEntry: DiskEntry,
         fromDiskEntry: DiskEntry = getRoot(diskEntry),
