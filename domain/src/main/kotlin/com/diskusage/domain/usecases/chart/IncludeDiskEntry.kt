53c79a26a177156dbd452f6df1d795696dadaa60
lukwol <wolanczyk.lukasz@gmail.com>
2022-06-30 09:14:34 +0200

    Add documentation for IncludeDiskEntry

--- domain/src/main/kotlin/com/diskusage/domain/usecases/chart/IncludeDiskEntry.kt
+++ domain/src/main/kotlin/com/diskusage/domain/usecases/chart/IncludeDiskEntry.kt
@@ -9,11 +9,26 @@ class IncludeDiskEntry(
     private val getDepth: GetDepth,
     private val getRoot: GetRoot,
 ) {
+
+    /**
+     * Checks whether given [diskEntry] should be included when drawing chart starting from [fromDiskEntry].
+     *
+     * To be included in the chart [diskEntry] can't be too small and too deeply nested.
+     *
+     * @param diskEntry [DiskEntry] for which inclusion will be checked
+     * @param fromDiskEntry [DiskEntry] for which the chart will be drawn, uses root if not given
+     *
+     * @return answer if [diskEntry] is included in chart
+     */
     operator fun invoke(
         diskEntry: DiskEntry,
         fromDiskEntry: DiskEntry = getRoot(diskEntry),
     ) = checkSizeInRange(diskEntry, fromDiskEntry) && checkDepthInRange(diskEntry, fromDiskEntry)
 
+    /**
+     * Check if size of [diskEntry] is not too small.
+     * Items that are too small are not included in the chart.
+     */
     private fun checkSizeInRange(
         diskEntry: DiskEntry,
         fromDiskEntry: DiskEntry,
@@ -22,6 +37,10 @@ class IncludeDiskEntry(
         return (size.takeIf(Double::isFinite)?.toFloat() ?: 0f) >= Constants.MinChartItemSize
     }
 
+    /**
+     * Check if [diskEntry] is not too deeply nested, starting from [fromDiskEntry].
+     * Items that are too deeply nested are not included in the chart.
+     */
     private fun checkDepthInRange(
         diskEntry: DiskEntry,
         fromDiskEntry: DiskEntry,
