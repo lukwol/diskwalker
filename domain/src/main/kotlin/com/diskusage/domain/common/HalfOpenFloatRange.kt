c56f4b900b05169015aded4741853807f0faa89f
lukwol <wolanczyk.lukasz@gmail.com>
2022-07-04 12:27:02 +0200

    Add documentation for HalfOpenFloatRange

--- domain/src/main/kotlin/com/diskusage/domain/common/HalfOpenFloatRange.kt
+++ domain/src/main/kotlin/com/diskusage/domain/common/HalfOpenFloatRange.kt
@@ -1,10 +1,25 @@
 package com.diskusage.domain.common
 
+/**
+ * Represents a half-open range of floating point numbers.
+ * Includes [start] but excludes [end] values.
+ * @see ClosedFloatingPointRange
+ */
 data class HalfOpenFloatRange(
     val start: Float,
     val end: Float,
 ) {
+    /**
+     * Checks if [provided number][other] is greater than or equal than [start] and less than [end].
+     *
+     * If the [end] value is less than or equal to [start] value than [provided number][other] will never be contained within the range.
+     */
     operator fun contains(other: Float) = start <= other && other < end
 }
 
-infix fun Float.until(to: Float) = HalfOpenFloatRange(this, to)
+/**
+ * Creates [HalfOpenFloatRange] from `this` value up to but excluding the specified [to] value.
+ */
+infix fun Float.until(to: Float): HalfOpenFloatRange {
+    return HalfOpenFloatRange(this, to)
+}
