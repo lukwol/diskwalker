ce7e19559fd9f752ea27dea93b324d2ef0102bfd
lukwol <wolanczyk.lukasz@gmail.com>
2022-07-07 12:09:54 +0200

    Rename FileSizeServiceFFI to FileSizeServiceImpl

--- libraries/support/src/main/kotlin/com/diskusage/libraries/support/di/SupportLibraryModule.kt
+++ libraries/support/src/main/kotlin/com/diskusage/libraries/support/di/SupportLibraryModule.kt
@@ -1,11 +1,11 @@
 package com.diskusage.libraries.support.di
 
 import com.diskusage.domain.services.FileSizeService
-import com.diskusage.libraries.support.filesize.FileSizeServiceFFI
+import com.diskusage.libraries.support.filesize.FileSizeServiceImpl
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val supportLibraryModule = module {
-    singleOf(::FileSizeServiceFFI) { bind<FileSizeService>() }
+    singleOf(::FileSizeServiceImpl) { bind<FileSizeService>() }
 }
--- libraries/support/src/main/kotlin/com/diskusage/libraries/support/filesize/FileSizeServiceImpl.kt
+++ libraries/support/src/main/kotlin/com/diskusage/libraries/support/filesize/FileSizeServiceImpl.kt
@@ -2,7 +2,7 @@ package com.diskusage.libraries.support.filesize
 
 import com.diskusage.domain.services.FileSizeService
 
-class FileSizeServiceFFI : FileSizeService {
+class FileSizeServiceImpl : FileSizeService {
     external override fun sizeOnDisk(path: String): Long
     external override fun sizeOnDiskFast(path: String): Long
 }
