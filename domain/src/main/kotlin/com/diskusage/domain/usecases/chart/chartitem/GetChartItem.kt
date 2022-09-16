d222c440ffcdff2470a5bfcfb4c44e23b7920686
lukwol <wolanczyk.lukasz@gmail.com>
2022-09-16 13:46:26 +0200

    Properly display color of list items

--- domain/src/main/kotlin/com/diskusage/domain/usecases/chart/chartitem/GetChartItem.kt
+++ domain/src/main/kotlin/com/diskusage/domain/usecases/chart/chartitem/GetChartItem.kt
@@ -1,26 +1,34 @@
 package com.diskusage.domain.usecases.chart.chartitem
 
+import androidx.compose.ui.graphics.Color
 import com.diskusage.domain.model.ChartItem
 import com.diskusage.domain.model.DiskEntry
+import com.diskusage.domain.usecases.chart.IncludeDiskEntry
 import com.diskusage.domain.usecases.chart.chartitem.arc.GetArc
 import com.diskusage.domain.usecases.diskentry.GetRoot
 
 // TODO: Add tests and documentation
 
+/**
+ * If [ChartItem] won't [be visible on chart][includeDiskEntry], set's it's [alpha][Color.alpha] value to **0f**.
+ */
 class GetChartItem(
     private val getRoot: GetRoot,
     private val getArc: GetArc,
-    private val getColor: GetColor
+    private val getColor: GetColor,
+    private val includeDiskEntry: IncludeDiskEntry
 ) {
     operator fun invoke(
         diskEntry: DiskEntry,
         fromDiskEntry: DiskEntry = getRoot(diskEntry)
     ): ChartItem {
         val arc = getArc(diskEntry, fromDiskEntry)
+        val color = getColor(diskEntry, fromDiskEntry, arc)
+        val isIncluded = includeDiskEntry(diskEntry, fromDiskEntry)
         return ChartItem(
             diskEntry = diskEntry,
             arc = arc,
-            color = getColor(diskEntry, fromDiskEntry, arc)
+            color = color.copy(alpha = if (isIncluded) 1f else 0f)
         )
     }
 }
--- domain/src/main/kotlin/com/diskusage/domain/usecases/chart/chartitem/GetColor.kt
+++ domain/src/main/kotlin/com/diskusage/domain/usecases/chart/chartitem/GetColor.kt
@@ -5,7 +5,6 @@ import androidx.compose.ui.graphics.ExperimentalGraphicsApi
 import com.diskusage.domain.common.Constants.Chart.MaxArcsDepth
 import com.diskusage.domain.model.Arc
 import com.diskusage.domain.model.DiskEntry
-import com.diskusage.domain.usecases.chart.IncludeDiskEntry
 import com.diskusage.domain.usecases.chart.chartitem.arc.GetArc
 import com.diskusage.domain.usecases.diskentry.GetDepth
 import com.diskusage.domain.usecases.diskentry.GetRoot
@@ -17,12 +16,9 @@ import com.diskusage.domain.usecases.diskentry.GetRoot
  *
  * When `diskEntry` is a [directory][DiskEntry.Type.Directory] color is computed based on [arc][getArc] and [depth][getDepth].
  * Uses `precalculatedArc` is it's provided.
- *
- * If `diskEntry` won't [be visible on chart][includeDiskEntry], set's it's [alpha][Color.alpha] value to **0f**.
  */
 class GetColor(
     private val getRoot: GetRoot,
-    private val includeDiskEntry: IncludeDiskEntry,
     private val getDepth: GetDepth,
     private val getArc: GetArc
 ) {
@@ -31,26 +27,20 @@ class GetColor(
         diskEntry: DiskEntry,
         fromDiskEntry: DiskEntry = getRoot(diskEntry),
         precalculatedArc: Arc = getArc(diskEntry, fromDiskEntry)
-    ): Color {
-        val color = when (diskEntry.type) {
-            DiskEntry.Type.File -> Color.hsl(
-                hue = 0f,
-                saturation = 0f,
-                lightness = 0.35f
+    ) = when (diskEntry.type) {
+        DiskEntry.Type.File -> Color.hsl(
+            hue = 0f,
+            saturation = 0f,
+            lightness = 0.35f
+        )
+
+        DiskEntry.Type.Directory -> with(precalculatedArc) {
+            val depth = getDepth(diskEntry, fromDiskEntry)
+            Color.hsl(
+                hue = (angleRange.end).coerceIn(0f, 360f),
+                saturation = ((angleRange.end / 360f) * 0.4f).coerceIn(0f, 0.4f),
+                lightness = (0.7f - (depth.toFloat() / MaxArcsDepth) * 0.4f).coerceIn(0.3f, 0.7f)
             )
-            DiskEntry.Type.Directory -> with(precalculatedArc) {
-                val depth = getDepth(diskEntry, fromDiskEntry)
-                Color.hsl(
-                    hue = (angleRange.end).coerceIn(0f, 360f),
-                    saturation = ((angleRange.end / 360f) * 0.4f).coerceIn(0f, 0.4f),
-                    lightness = (0.7f - (depth.toFloat() / MaxArcsDepth) * 0.4f).coerceIn(0.3f, 0.7f)
-                )
-            }
-        }
-        return if (includeDiskEntry(diskEntry, fromDiskEntry)) {
-            color
-        } else {
-            color.copy(alpha = 0f)
         }
     }
 }
