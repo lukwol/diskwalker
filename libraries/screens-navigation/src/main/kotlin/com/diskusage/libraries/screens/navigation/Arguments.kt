1c7cb9255d24a38e96ca6b7a354876165a34e9c5
lukwol <wolanczyk.lukasz@gmail.com>
2022-10-30 12:26:25 +0100

    Add Arguments and ScreensController documentation

--- libraries/screens-navigation/src/main/kotlin/com/diskusage/libraries/screens/navigation/Arguments.kt
+++ libraries/screens-navigation/src/main/kotlin/com/diskusage/libraries/screens/navigation/Arguments.kt
@@ -1,3 +1,6 @@
 package com.diskusage.libraries.screens.navigation
 
+/**
+ * Arguments passed when navigating to next screen.
+ */
 typealias Arguments = Any
--- libraries/screens-navigation/src/main/kotlin/com/diskusage/libraries/screens/navigation/ScreensController.kt
+++ libraries/screens-navigation/src/main/kotlin/com/diskusage/libraries/screens/navigation/ScreensController.kt
@@ -2,12 +2,36 @@ package com.diskusage.libraries.screens.navigation
 
 import androidx.compose.runtime.mutableStateOf
 
+/**
+ * Manages navigation between the screens declared when building [ScreensNavigation].
+ */
 interface ScreensController {
+
+    /**
+     * List of current [ScreenRoute] on the stack.
+     * For the last [ScreenRoute] screen will be displayed.
+     */
     val routes: List<ScreenRoute>
+
+    /**
+     * Pushes new [ScreenRoute] on the [routes] stack.
+     *
+     * @param route [ScreenRoute] to navigate to
+     * @param arguments optional [Arguments] passed when navigating to [route]
+     */
     fun push(route: ScreenRoute, arguments: Arguments? = null)
+
+    /**
+     * Pops back to previous [ScreenRoute] on the [routes] stack.
+     *
+     * @param upToRoute if provided optionally pop multiple routes [upToRoute]
+     */
     fun pop(upToRoute: ScreenRoute? = null)
 }
 
+/**
+ * Actual implementation of the [ScreensController]
+ */
 internal class ScreensControllerImpl(startRoute: ScreenRoute) : ScreensController {
     internal val routesState = mutableStateOf(listOf(RouteWithArguments(startRoute)))
 
@@ -17,6 +41,15 @@ internal class ScreensControllerImpl(startRoute: ScreenRoute) : ScreensControlle
         routesState.value += RouteWithArguments(route, arguments)
     }
 
+    /**
+     * Pops back to previous [ScreenRoute] on the [routes] stack.
+     *
+     * @param upToRoute if provided optionally pop multiple routes [upToRoute]
+     *
+     * @throws IllegalArgumentException if there is no [upToRoute] on the stack
+     * or [upToRoute] is the last route on the stack
+     * @throws IllegalStateException if it's about to pop last route on the [routes] stack
+     */
     override fun pop(upToRoute: ScreenRoute?) {
         return when {
             upToRoute != null && upToRoute !in routes -> throw IllegalArgumentException("There is no $upToRoute on the stack")
@@ -28,6 +61,9 @@ internal class ScreensControllerImpl(startRoute: ScreenRoute) : ScreensControlle
     }
 }
 
+/**
+ * No-op implementation of the [ScreensController]
+ */
 internal object ScreensControllerNoOp : ScreensController {
     override val routes: List<ScreenRoute> = emptyList()
 
