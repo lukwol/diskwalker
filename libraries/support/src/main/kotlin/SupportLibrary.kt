58b6b5ee85bd2fcd06392c52b57e6d18e0ee6696
lukwol <wolanczyk.lukasz@gmail.com>
2022-11-28 10:33:28 +0100

    Support both architectures on macOS

--- libraries/support/src/main/kotlin/SupportLibrary.kt
+++ libraries/support/src/main/kotlin/SupportLibrary.kt
@@ -2,7 +2,11 @@ import java.io.File
 
 object SupportLibrary {
     fun loadLibrary() {
-        val inputStream = javaClass.getResourceAsStream("libsupport.dylib")
+        val filename = when (System.getProperty("os.arch")) {
+            "aarch64" -> "libsupport-aarch64-apple-darwin.dylib"
+            else -> "libsupport-x86_64-apple-darwin.dylib"
+        }
+        val inputStream = javaClass.getResourceAsStream(filename)
         val file = File.createTempFile("disk-usage-", "-jni-lib")
         inputStream?.readBytes()?.let { file.writeBytes(it) }
         System.load(file.absolutePath)
