aeff15c89d5d36e91bb4213d38a92aceb9e2de69
lukwol <wolanczyk.lukasz@gmail.com>
2022-12-11 17:45:45 +0100

    Use DiskButton

--- presentation/src/main/kotlin/com/diskusage/presentation/screens/dashboard/DashboardScreen.kt
+++ presentation/src/main/kotlin/com/diskusage/presentation/screens/dashboard/DashboardScreen.kt
@@ -3,9 +3,7 @@ package com.diskusage.presentation.screens.dashboard
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -14,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.diskusage.domain.common.Constants
 import com.diskusage.presentation.navigation.AppRoutes
+import com.diskusage.presentation.screens.dashboard.components.DiskButton
 import io.github.anvell.async.Success
 import io.github.anvell.async.Uninitialized
 import io.github.lukwol.screens.navigation.LocalScreensController
@@ -32,13 +31,12 @@ fun DashboardScreen(viewModel: DashboardViewModel) {
             .background(MaterialTheme.colors.background)
             .fillMaxSize()
     ) {
-        Button(
-            enabled = viewState.selectedDiskEntry == Uninitialized,
-            onClick = {
-                viewModel.selectScannedPath(Path.of(Constants.Disk.RootDiskPath))
-            }
+        DiskButton(
+            title = "Macintosh HD",
+            progress = 0.8f,
+            enabled = viewState.selectedDiskEntry == Uninitialized
         ) {
-            Text("Scan disk")
+            viewModel.selectScannedPath(Path.of(Constants.Disk.RootDiskPath))
         }
     }
 
