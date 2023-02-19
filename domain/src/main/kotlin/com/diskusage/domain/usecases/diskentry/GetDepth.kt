0429c570ffce29858608062a551bf130123bf7bf
lukwol <wolanczyk.lukasz@gmail.com>
2023-02-19 09:49:58 +0100

    Update `GetDepth` to new model

--- domain/src/main/kotlin/com/diskusage/domain/usecases/diskentry/GetDepth.kt
+++ domain/src/main/kotlin/com/diskusage/domain/usecases/diskentry/GetDepth.kt
@@ -1,6 +1,7 @@
 package com.diskusage.domain.usecases.diskentry
 
 import com.diskusage.domain.model.DiskEntry
+import java.nio.file.Path
 
 /**
  * Calculates depth level, how much nested `diskEntry` is compared to `fromDiskEntry`
@@ -14,6 +15,11 @@ class GetDepth(
         fromDiskEntry: DiskEntry = getRoot(diskEntry)
     ) = depth(diskEntry, fromDiskEntry)
 
+    operator fun invoke(
+        path: Path,
+        fromPath: Path = getRoot(path)
+    ) = depth(path, fromPath)
+
     private fun depth(diskEntry: DiskEntry, fromDiskEntry: DiskEntry, depth: Int = 1): Int =
         when (getRelationship(diskEntry, fromDiskEntry)) {
             DiskEntry.Relationship.Identity -> depth
@@ -21,5 +27,16 @@ class GetDepth(
             DiskEntry.Relationship.Ancestor, DiskEntry.Relationship.Sibling -> {
                 depth(diskEntry.parent!!, fromDiskEntry, depth + 1)
             }
+        }
+
+    private fun depth(path: Path, fromPath: Path, depth: Int = 1): Int =
+        when (getRelationship(path, fromPath)) {
+            DiskEntry.Relationship.Identity -> depth
+            DiskEntry.Relationship.Unrelated, DiskEntry.Relationship.Descendant -> 0
+            DiskEntry.Relationship.Ancestor, DiskEntry.Relationship.Sibling -> depth(
+                path = path.parent,
+                fromPath = fromPath,
+                depth = depth + 1
+            )
         }
 }
