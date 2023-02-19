f8661750d17cd55f26ebff1cfee6332f8c4ed821
lukwol <wolanczyk.lukasz@gmail.com>
2023-02-19 20:16:54 +0100

    Don't ask for children if path is file

--- domain/src/main/kotlin/com/diskusage/domain/usecases/scan/GetChildren.kt
+++ domain/src/main/kotlin/com/diskusage/domain/usecases/scan/GetChildren.kt
@@ -1,15 +1,23 @@
 package com.diskusage.domain.usecases.scan
 
+import com.diskusage.domain.model.scan.PathInfo
 import com.diskusage.domain.services.ScanRepository
 import java.nio.file.Path
 
-class GetChildren(private val repository: ScanRepository) {
+class GetChildren(
+    private val repository: ScanRepository,
+    private val getPathInfo: GetPathInfo
+) {
 
-    operator fun invoke(path: Path) = repository
-        .runCatching { children(path) }
-        .onFailure {
-            println("Failed to get children for path $path")
-        }
-        .getOrNull()
-        .orEmpty()
+    operator fun invoke(path: Path) = when (getPathInfo(path)) {
+        is PathInfo.Directory ->
+            repository
+                .runCatching { children(path) }
+                .onFailure {
+                    println("Failed to get children for path $path")
+                }
+                .getOrNull()
+                .orEmpty()
+        is PathInfo.File -> emptySet()
+    }
 }
