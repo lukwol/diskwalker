565d5738d727b3488f9bbf03cac58cfe331f82cf
lukwol <wolanczyk.lukasz@gmail.com>
2023-03-02 15:12:09 +0100

    Generate protobuf classes in data module

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,11 +1,16 @@
+import com.google.protobuf.gradle.id
+
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.protobuf)
 }
 
 dependencies {
     implementation(projects.domain)
 
+    implementation(libs.protobuf.javalite)
+    implementation(libs.protobuf.kotlin.lite)
     implementation(libs.koin.core)
     implementation(libs.async.core)
     implementation(libs.coroutines.core)
@@ -16,6 +21,33 @@ dependencies {
     testImplementation(libs.koin.test)
     testImplementation(libs.koin.junit5)
     testImplementation(libs.coroutines.test)
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDirs("${rootProject.projectDir.path}/protos")
+        }
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:${libs.versions.protobuf.get()}"
+    }
+
+    generateProtoTasks {
+        all().forEach {
+            it.builtins {
+                getByName("java") {
+                    option("lite")
+                }
+                id("kotlin") {
+                    option("lite")
+                }
+            }
+        }
+    }
 }
 
 tasks.test {
